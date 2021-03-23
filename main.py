1de4e8fd79f198596a7c9a37d056d495db2aaa95
andrewmadea@gmail.com <MliRP!pr0>
2021-03-22 23:47:45 -0400

    Fixed an issue where the number of entries or exits would be off by >100k.  If an entry only has one entry, then it is ignored since the find_difference method needs more than one number to calculate an accurate number.

--- main.py
+++ main.py
@@ -1,45 +1,94 @@
 import pandas as pd
 from datetime import datetime, timedelta
 import matplotlib as plt
+from urllib.error import HTTPError
+import plotly.express as px
+import numpy as np
 
 '''
 @ returns the date of the last saturday
 '''
 def findLastSaturdayDate():
+
     saturday = None
     today = datetime.today().date()
     day = today.isoweekday()
-    if day == 6:
-        saturday = today
-    elif day == 7:  # Today is Sunday.  Sunday is 7.  Sunday - 1 is saturday
-        saturday = today - timedelta(days=1)
+
+    # Ternary Operator to choose the previous saturday
+    # -(day % 6 + 1) changes isoformat for each day to the previous saturday
+    saturday = (today + timedelta(days=-7)) if day == 6 else today + timedelta(days=-(day % 6 + 1))
+    return saturday
+
+def pathRidesPerDay(dataframe):
+
+    final = pd.DataFrame(data={}, columns=['DATE', 'STATION', 'ENTRIES', 'EXITS'])
+    dataframe = dataframe[dataframe['DIVISION']=='PTH']
+    for date in dataframe['DATE'].unique():
+        for station in dataframe['STATION'].unique():
+            entries = 0
+            exits = 0
+            for scp in dataframe['SCP'].unique():
+                temp = dataframe[(dataframe['DATE'] == date) & (dataframe['STATION'] == station) & (dataframe['SCP'] == scp)]
+                if len(temp) != 0:
+                    entryList = temp['ENTRIES'].to_list()
+                    exitList = temp['EXITS'].to_list()
+                    entries = entries + find_differences(entryList)
+                    exits = exits + find_differences(exitList)
+
+            final = final.append(other={'DATE': date,
+                                        'STATION': station,
+                                        'ENTRIES': entries,
+                                        'EXITS': exits
+                                        }, ignore_index=True)
+    return final
+
+def get_last_mta_dataframe():
+    date = findLastSaturdayDate()
+    dateString = date.isoformat().replace('-', '')[2:]
+    print('Date To be used: ', date, '\ndate string: ',dateString)
+    try:
+        baseURL = "http://web.mta.info/developers/data/nyct/turnstile/turnstile_{}.txt".format(dateString)
+        df = pd.read_csv(baseURL)
+    except HTTPError as err:
+        print('something went wrong')
+    finally:
+        # The last column is supposed to be 'EXITS' but it has alot of spaces after it right now
+        errorColumn = df.columns[-1]
+        df.rename({errorColumn: 'EXITS'}, axis=1, inplace=True)
+        return df
+
+def find_differences(dlist):
+
+    if len(dlist) <= 1:
+        return 0
+    diff = 0
+    diff_list = []
+    t = [dlist[0]]
+    for n in range(1, len(dlist)):
+        if t[-1] <= dlist[n]:
+            t.append(dlist[n])
+        else:
+            diff_list.append(t[-1] - t[0])
+            t.clear()
+            t.append(dlist[n])
+    if len(t) == 1:
+        diff_list.append(t[0])
     else:
-        saturday = today - timedelta(days=(day+1))
-    return saturday.isoformat().replace('-', '')[2:]
+        diff_list.append(t[-1] - t[0])
+    return diff
 
 if __name__ == '__main__':
-    dateString = findLastSaturdayDate()
-    print('Date To be used: ', dateString)
-    baseURL = "http://web.mta.info/developers/data/nyct/turnstile/turnstile_{}.txt".format(dateString)
-    df = pd.read_csv(baseURL)
 
-    '''
-    The errorColumn is known as 'EXITS              
-    It has a lot of spaces at the end of the string which makes the call
-    df['EXITS'] or the like like invalid.  These two lines fix column label as 'EXITS'
-    '''
-    errorColumn = df.columns[df.columns.size-1]
-    df.rename({errorColumn: 'EXITS'}, axis=1, inplace=True)
+    df = get_last_mta_dataframe()
+    pathTrains = pathRidesPerDay(df)
+    pathGroupedEntries = pathTrains.groupby(['STATION', 'DATE'])['ENTRIES'].sum().reset_index()
+    pathGroupedExits = pathTrains.groupby(['STATION', 'DATE'])['EXITS'].sum().reset_index()
 
-    # Just screwing around with this part.  Getting the times the MTA updates their info.
-    # Getting the hour position and converting it to an int
-    df['TIME'] = df['TIME'].str.slice(0, 2)
-    df[['TIME']] = df['TIME'].astype('int')
+    figs = [px.line(pathGroupedEntries, x='DATE', y='ENTRIES', color='STATION', title='ENTRIES for the PATH Train'),
+            px.line(pathGroupedExits, x='DATE', y='EXITS', color='STATION', title='Exits for the PATH')]
+
+    for f in figs:
+        f.show()
 
     print('Data Types: ', df.dtypes)
     print('Shape: ', df.shape)
-
-    # Creating histogram
-    df['TIME'].plot(kind="hist", xlabel='Frequency',ylabel='Hour of the day',title='Frequency of Hour of MTA Reporting')
-
-    plt.pyplot.show()
