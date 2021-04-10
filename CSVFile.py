3f5d4b0526eb4501578c84871b019a9c64b5abdd
Andrew <andrewmadea@gmail.com>
2021-04-09 22:30:51 -0400

    Created a method that iterates over different eps and min_samples for the DBSCAN analyze method.  Automatically writes to a .csv file in /data

--- CSVFile.py
+++ CSVFile.py
@@ -2,6 +2,64 @@ import DataframeCreation as dfc
 import DataframeModification as dfm
 import pandas as pd
 import AnalyzingGroups as ag
+import warnings
+
+
+def analyze_path_trains(fileName):
+    d = dfc.get_latest_mta_dataframe()
+    d = d[d['DIVISION'] == 'PTH']
+    stations = d['STATION'].unique().tolist()
+    pathTrains = pd.DataFrame()
+    data = pd.read_csv(fileName)
+    for i in data.index:
+        if data.loc[i, 'STATION'] in stations:
+            pathTrains = pathTrains.append(data.loc[i, ])
+    fileName = fileName.split('_')
+    pathTrains.to_csv('data/path_trains_dbscan_analysis_{}.csv'.format(fileName[2]))
+
+
+def create_rides_over_time_csv(df):
+    ridesPerDay = dfc.find_total_rides_per_day(df)
+    ridesPerDay.sort_values(by='DATE', inplace=True)
+    d = dfm.create_date_dictionary_string(df)
+    dates = sorted(list(d.keys()))
+    fromDate = d.get(dates[0])
+    toDate = d.get(dates[-1])
+
+    fileName = 'data/rides_over_time_{}_to_{}.csv'.format(fromDate, toDate)
+    print('Trying to save to file: ', fileName)
+    ridesPerDay.to_csv(fileName)
+
+
+def analyze_with_different_dbscan_params(a, b, c, x, y,):
+    warnings.filterwarnings('ignore')
+    rawData = dfc.get_n_latest_mta_dataframes(5)
+    saturdays = dfc.find_saturday_dates_strings(5)
+    iterations = (b-a)/c * (y - x)
+    print('total DBSCAN param iterations:', iterations)
+    current = 0
+
+    for eps in range(a, b, c):
+        for min_samples in range(x, y):
+            analyzedData = ag.analyze(rawData, eps=eps, min_samples=min_samples)
+            fileName = 'data/dbscan_data_outputs/dbscan_eps={}_min_samples={}_from_{}_to_{}.csv'.format(
+                eps, min_samples, saturdays[-1], saturdays[0])
+            analyzedData.to_csv(fileName)
+            current += 1
+            print(current, '/', iterations)
+
+
+def analyze_append_new_data_to_csv(fileName):
+    newData = dfc.get_latest_mta_dataframe()
+    analyzedNewData = ag.analyze(newData)
+    oldData = pd.read_csv(fileName)
+    newTotal = pd.concat([newData, oldData])
+
+    saturday = dfc.find_last_saturday_string()
+    newName = 'pathTrains_{}_to_{}.csv'.format(fileName.split('_')[1], saturday[0])
+    print('Writing out to file: ', newName)
+    newTotal.to_csv(newName)
+
 
 def clean_up_columns(fileName):
     data = pd.read_csv(fileName)
@@ -59,6 +117,23 @@ def parse_non_path_trains(number):
 def sort_by_date(df, fileName):
     df.sort_values(by='DATE', inplace=True)
     df.to_csv(fileName)
+
+
+def analyze_n_dataframes_by_DBSCAN(number):
+    data = dfc.get_n_latest_mta_dataframes(number)
+    saturdays = dfc.find_saturday_dates_strings(number)
+    data = data[data['DESC'] == 'REGULAR']
+    analyzedData = ag.analyze(data)
+    print(ag.count_negative_groups(analyzedData))
+    analyzedData.to_csv('data/dbscan_analysis_{}_to_{}.csv'.format(saturdays[-1], saturdays[0]))
+
+
+def analyze_latest_dataframe_by_DBSCAN_():
+    data = dfc.get_latest_mta_dataframe()
+    saturday = dfc.find_last_saturday_string()
+    data = data[data['DESCRIPTION'] == 'REGULAR']
+    analyzedData = ag.analyze(data)
+    analyzedData.to_csv('data/dbscan_analysis_{}.csv'.format(saturday))
 
 
 def analyze_latest_dataframe_by_DBSCAN(station):
