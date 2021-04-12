83986f8c7acd81cefb680a23c7668b1dbc1f6f42
Andrew <andrewmadea@gmail.com>
2021-04-12 01:19:10 -0400

    Method graphs rides over time as an animation

--- Graphs.py
+++ Graphs.py
@@ -1,19 +1,26 @@
 import plotly.express as px
 import DataframeModification as dfm
-#from multipledispatch import dispatch
 
 
-def graph_scatter(df):
-    graph_scatter(df=df, title='Entries v.s. Exits for PATH Stations')
+def graph_rides_over_time(data):
+    data.sort_values(by='DATE', inplace=True)
+    fig = px.scatter(data, x='EXITS', y='ENTRIES', animation_frame='DATE', color='STATION', title='Rides Over Time',
+                     range_x=[-10, 50000], range_y=[-10, 50000])
+    fig.show()
 
 
-def graph_scatter(df, title):
+def graph_entries_v_exits(df):
+    graph_entries_v_exits(df=df, title='Entries v.s. Exits for Stations')
+
+
+def graph_entries_v_exits(df, title):
     if 'ISOWEEKDAY' not in df.columns:
         df = dfm.get_isodate_number(df)
 
-    fig = px.scatter(df, x='ENTRIES', y='EXITS', color='STATION',
-                     symbol='ISOWEEKDAY', title=title)
+    fig = px.scatter(df, x='EXITS', y='ENTRIES', color='STATION',
+                     title=title)
 
+    print("About to show the chart...")
     fig.show()
 
 
@@ -26,3 +33,11 @@ def graph_pathTrains_over_time(pathTrains):
 
     for f in figs:
         f.show()
+
+
+def graph_entry_to_exit_ratio_overtime(df):
+    if 'ENTRIES/EXITS' not in df.columns:
+        df = dfm.entry_exit_ratio(df)
+    df.sort_values(by='DATE', inplace=True)
+    fig = px.line(df, x='DATE', y='ENTRIES/EXITS', color='STATION', title='Entry To Exit Ratio Over Time')
+    fig.show()
